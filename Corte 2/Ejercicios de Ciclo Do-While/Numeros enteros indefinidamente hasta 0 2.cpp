97828018c01d065bdb35fb60228032f3cf124236
Jessica Catama <jessica.catamac@uniagustiniana.edu.co>
2025-09-10 21:07:51 -0500

    Add files via upload

---
+++ Corte 2/Ejercicios de Ciclo Do-While/Numeros enteros indefinidamente hasta 0 2.cpp
@@ -0,0 +1,20 @@
+#include "iostream"
+using namespace std;
+
+int main(){
+	
+system ("color f6");
+
+int a, b;
+	cout<<"Digita un número: "<< endl;
+	cin>> a;
+	
+	do{ 
+	cout<<" "<< a;
+	cout<< endl;
+	a = a - b;
+}
+	while (a >= 0);
+	
+system("PAUSE");
+}
